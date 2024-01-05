2c7a731af5cc98ae1d01f5276814fd5ad724c1bd
Yong Ying <yoying@microsoft.com>
2024-01-05 15:32:43 +0800

    add api'

---
+++ api/data/__init__.py
@@ -0,0 +1,41 @@
+import datetime
+import logging
+import os
+import requests
+import json
+import pyodbc
+from datetime import datetime as dt
+import azure.functions as func
+
+#AZURE_CONN_STRING = str(os.environ["AzureSQLConnectionString"])
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    result = {}
+    
+    try:
+        rid = int(req.params['rid'])
+        gid = int(req.params['gid'])
+    except ValueError:
+        rid = 0
+        gid = 0
+    '''
+    try: 
+        conn = pyodbc.connect(AZURE_CONN_STRING)
+        
+        with conn.cursor() as cursor:
+            cursor.execute(f"EXEC [web].[GetMonitoredBusData] ?, ?", rid, gid)
+
+            result = cursor.fetchone()[0]
+            
+            if result:
+                result = json.loads(result)                           
+            else:
+                result = {}     
+
+            logging.info(result)   
+        
+    finally:
+        cursor.close()
+    '''
+    result = f'get data with rid={rid} gid={gid}'
+    return func.HttpResponse(result)
