53a36e99d8261fbfa8763d1f4c38f52630dcbdbd
Yong Ying <yoying@microsoft.com>
2024-01-05 17:51:54 +0800

    fa v2

--- api/data/__init__.py
+++ api/data/__init__.py
@@ -1,41 +1,13 @@
-import datetime
-import logging
-import os
-import requests
-import json
-import pyodbc
-from datetime import datetime as dt
 import azure.functions as func
 
-#AZURE_CONN_STRING = str(os.environ["AzureSQLConnectionString"])
+import fastapi
 
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    result = {}
-    
-    try:
-        rid = int(req.params['rid'])
-        gid = int(req.params['gid'])
-    except ValueError:
-        rid = 0
-        gid = 0
-    '''
-    try: 
-        conn = pyodbc.connect(AZURE_CONN_STRING)
-        
-        with conn.cursor() as cursor:
-            cursor.execute(f"EXEC [web].[GetMonitoredBusData] ?, ?", rid, gid)
+app = fastapi.FastAPI()
 
-            result = cursor.fetchone()[0]
-            
-            if result:
-                result = json.loads(result)                           
-            else:
-                result = {}     
+@app.get("/api/data")
+async def index():
+    return {
+        "info": "Try /hello/Shivani for parameterized route.",
+    }
 
-            logging.info(result)   
-        
-    finally:
-        cursor.close()
-    '''
-    result = f'get data with rid={rid} gid={gid}'
-    return func.HttpResponse(result)
+
