80536364928cfeffdb7316dee6f39452206341b3
Yong Ying <yoying@microsoft.com>
2024-01-05 20:15:34 +0800

    v1 func

--- api/getname/__init__.py
+++ api/getname/__init__.py
@@ -1,16 +1,3 @@
-import azure.functions as func
-
-import fastapi
-
-app = fastapi.FastAPI()
-
-@app.get("/api/data")
-async def index():
-    return {
-        "info": "Try /hello/Shivani for parameterized route.",
-    }
-
-
 import logging
 
 import azure.functions as func
